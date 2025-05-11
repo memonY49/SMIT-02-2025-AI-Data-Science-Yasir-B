0dd16f9b8c09308122140118e866849616ddaed3
memonY49 <memony49@gmail.com>
2025-05-11 21:33:59 +0500

    Week10

---
+++ week2BatchC.py
@@ -0,0 +1,14 @@
+name = "Yasir Nawaz"
+phone = 923000390000
+cnic = "41237-899804-4"
+email = "Gmail@gmail.com"
+password = "abc123"
+
+
+if email == "Gmail@g2mail.com":
+    print("Name: ",name)
+    print("Cnic: ",cnic)
+    print("Phone: ",phone)
+    print("Email: ",email)
+else:
+    print("Invailed User")
